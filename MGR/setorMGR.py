ad5737d7f5cb03eb86187035025a8335e1eaa034
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-08-23 09:49:02 -0300

    setorMGR
    função RegistrarAndamento

--- MGR/setorMGR.py
+++ MGR/setorMGR.py
@@ -1,4 +1,20 @@
+import DAO.setorDAO 
+from ENTITIES.fPedido import fPedido
 class setor:
     def __init__(self, ids:int, nome):
         self.id = ids
         self.nome = nome
+
+    def RegistrarAndamento(status:str, data:int):
+        try:
+            fluxo = fPedido()
+            fluxo.setData(data)
+            fluxo.setStatus(status)
+            
+        except Exception as e:
+            return e
+
+    def RealizarRelatorio(self):
+
+
+    def ValidarPedido(self):
